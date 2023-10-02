0111688d87ffae7e4307d3badf5bb5683b346a01
Sarun Thongaram <sthongaram@third500.com>
2023-10-02 16:22:54 +0700

    create app

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,18 +1,16 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { FormsModule } from '@angular/forms';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { HeaderComponent } from './header/header.component';
+import { MyProjectsComponent } from './my-projects/my-projects.component';
+import { MyIntroductionComponent } from './my-introduction/my-introduction.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  declarations: [AppComponent, HeaderComponent, MyProjectsComponent, MyIntroductionComponent],
+  imports: [BrowserModule, AppRoutingModule, FormsModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/my-introduction/my-introduction.component.ts
@@ -0,0 +1,8 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'my-introduction',
+  templateUrl: './my-introduction.component.html',
+  styleUrls: ['./my-introduction.component.css'],
+})
+export class MyIntroductionComponent {}
